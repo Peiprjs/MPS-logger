b2cc9e96540938388e0802adba9f3bb28561e276
Piei <34306928+Piei@users.noreply.github.com>
2020-11-09 19:38:29 +0100

    Added some shit code. CLOSED the project

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const config = require("./config.json");
 const random = require('random');
 const client = new Discord.Client();
 const prefix = "!";
+client.login(config.BOT_TOKEN);
 
 console.log(`Logged in as ${client.user.tag}!`);
 
@@ -21,9 +22,11 @@ client.on("message", function(message) {
   }
 
   if (command === "fpverify") {
+    const fpok = new Discord.MessageEmbed()
+	.setColor('#0bfd00')
+	.setTitle('Registration successful!')
+	.setDescription('You have now been registered to the FP logging Trello. Please do not run this command again. Abusing this command can result in a bot blacklit and, as such, proper dismissal.')
 
-  }
-
-
-
-client.login(config.BOT_TOKEN);
+    channel.message.send(fpok)
+  };
+})
